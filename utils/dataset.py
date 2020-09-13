0d41cd048236b458cf2f45d90c6015c1e495b72a
Cute77 <goagoaleen@gmail.com>
2020-09-13 22:29:10 +0800

    update

--- utils/dataset.py
+++ utils/dataset.py
@@ -26,7 +26,7 @@ class BasicDataset(Dataset):
                transforms.RandomRotation(degrees=20),
                # transforms.RandomGrayscale(p=0.1),
                # transform.RandomResizedCrop(scale=(0.75, 1.25), size=512), 
-               transforms.Resize([512, 512]), 
+               transform.Resize([512, 512]), 
                # transforms.ToTensor(), 
                # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[1.0, 1.0, 1.0])
             ]) 
