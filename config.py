18b0fcc504435fc10d343cb4ed2109ec1ff4c076
Cute77 <joajoaleen@gmail.com>
2020-09-27 23:44:55 +0800

    update

--- config.py
+++ config.py
@@ -2,20 +2,24 @@ from os import listdir
 import shutil
 import random
 
-intputpath = 'ISIC-2017_Training_Data'
-outputpath = 'ISIC-2017_Training_Data_validation'
-dirname = 'ISIC-2017_Training_Part1_GroundTruth_validation'
+# intputpath = 'ISIC-2017_Training_Data'
+# outputpath = 'ISIC-2017_Training_Data_validation'
+# dirname = 'ISIC-2017_Training_Part1_GroundTruth_validation'
+inputpath = 'ISIC-2017_Training_Data_validation'
+outputpath = 'ISIC-2017_Training_Data_clean'
 name = []
 
-for filename in listdir(dirname):
+for filename in listdir(inputpath):
     line = 'ISIC_' + filename.split('_')[1] + '.jpg'
     name.append(line)
+
+namefinal = name[:5]
 
 # random.shuffle(name)
 # offset = int(len(name) * 0.1)
 # data = name[:offset]
 
-for line in name:
+for line in namefinal:
     src = intputpath + '/' + line
     print(src)
     dst = outputpath + '/' + line
